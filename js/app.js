05087c3025e39ae7769a98cc882b6a2d89c60670
Chris Gantt <gantt.art@gmail.com>
2021-02-15 14:25:38 -0800

    Added object literals for 4 more worldwide stores, and populated them with starter data from the assignment.

--- js/app.js
+++ js/app.js
@@ -10,7 +10,47 @@ let seattle = {
   },
 };
 
-// let stores = [seattle, tokyo, dubai, paris, lima];
+let tokyo = {
+  minHourlyCustomers: 3,
+  maxHourlyCustomers: 24,
+  averageCookiesPerCustomer: 1.2,
+  hourlyCookiesSold: [],
+  randomCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
+  },
+};
+
+let dubai = {
+  minHourlyCustomers: 11,
+  maxHourlyCustomers: 38,
+  averageCookiesPerCustomer: 3.7,
+  hourlyCookiesSold: [],
+  randomCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
+  },
+};
+
+let paris = {
+  minHourlyCustomers: 20,
+  maxHourlyCustomers: 38,
+  averageCookiesPerCustomer: 2.3,
+  hourlyCookiesSold: [],
+  randomCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
+  },
+};
+
+let lima = {
+  minHourlyCustomers: 2,
+  maxHourlyCustomers: 16,
+  averageCookiesPerCustomer: 4.6,
+  hourlyCookiesSold: [],
+  randomCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
+  },
+};
+
+let stores = [seattle, tokyo, dubai, paris, lima];
 
 function populateHourlyCookiesSold(store) {
   for (let i = 0; i < 14; i++) {
