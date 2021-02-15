cb3fbf64ddcaed791d519511e07a34c6c10792c8
Chris Gantt <gantt.art@gmail.com>
2021-02-15 14:21:39 -0800

    Created an object, Seattle, and created a function, populateHourlyCookiesSold, that does what it says it does for whatever store is passed in.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let seattle = {
+  minHourlyCustomers: 23,
+  maxHourlyCustomers: 65,
+  averageCookiesPerCustomer: 6.3,
+  hourlyCookiesSold: [],
+  randomCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
+  },
+};
+
+// let stores = [seattle, tokyo, dubai, paris, lima];
+
+function populateHourlyCookiesSold(store) {
+  for (let i = 0; i < 14; i++) {
+    let customers = store.randomCustomersPerHour();
+    let cookiesSold = Math.floor(customers * store.averageCookiesPerCustomer);
+    seattle.hourlyCookiesSold.push(cookiesSold);
+  }
+}
+
+populateHourlyCookiesSold(seattle);
+console.log(seattle);
