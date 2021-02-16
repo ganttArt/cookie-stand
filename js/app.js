89f70379d15dccd90994c09089803df4c0105311
Chris Gantt <gantt.art@gmail.com>
2021-02-16 10:39:19 -0800

    Replaced object literals for each store with objects declared using the constructor.

--- js/app.js
+++ js/app.js
@@ -14,65 +14,11 @@ function CookieStore(location, minHourlyCustomers, maxHourlyCustomers, averageCo
   };
 }
 
-let seattle = {
-  location: 'Seattle',
-  minHourlyCustomers: 23,
-  maxHourlyCustomers: 65,
-  averageCookiesPerCustomer: 6.3,
-  hourlyCookiesSold: [],
-  totalCookiesSold: 0,
-  randomCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-  },
-};
-
-let tokyo = {
-  location: 'Tokyo',
-  minHourlyCustomers: 3,
-  maxHourlyCustomers: 24,
-  averageCookiesPerCustomer: 1.2,
-  hourlyCookiesSold: [],
-  totalCookiesSold: 0,
-  randomCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-  },
-};
-
-let dubai = {
-  location: 'Dubai',
-  minHourlyCustomers: 11,
-  maxHourlyCustomers: 38,
-  averageCookiesPerCustomer: 3.7,
-  hourlyCookiesSold: [],
-  totalCookiesSold: 0,
-  randomCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-  },
-};
-
-let paris = {
-  location: 'Paris',
-  minHourlyCustomers: 20,
-  maxHourlyCustomers: 38,
-  averageCookiesPerCustomer: 2.3,
-  hourlyCookiesSold: [],
-  totalCookiesSold: 0,
-  randomCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-  },
-};
-
-let lima = {
-  location: 'Lima',
-  minHourlyCustomers: 2,
-  maxHourlyCustomers: 16,
-  averageCookiesPerCustomer: 4.6,
-  hourlyCookiesSold: [],
-  totalCookiesSold: 0,
-  randomCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
-  },
-};
+let seattle = new CookieStore('Seattle', 23, 65, 6.3);
+let tokyo = new CookieStore('Tokyo', 3, 24, 1.2);
+let dubai = new CookieStore('Dubai', 11, 38, 3.7);
+let paris = new CookieStore('Paris', 20, 38, 2.3);
+let lima = new CookieStore('Lima', 2, 16, 4.6);
 
 let stores = [seattle, tokyo, dubai, paris, lima];
 
